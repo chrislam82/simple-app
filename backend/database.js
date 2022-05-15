1a2fea91f9222c61126381019874a28051ac1432
JohnSmith72 <chris.sc.lam29@gmail.com>
2022-05-15 18:55:34 +1000

    Refactored backend. Added ES module compatability

---
+++ backend/database.js
@@ -0,0 +1,97 @@
+import fs from 'fs';
+import lockFile from 'proper-lockfile';
+
+const DATASTORE = './msgs.json';
+
+let msgData = {};
+
+const retryOptions = {
+  retries: 3,
+  randomize: true,
+}
+
+/************************************************************************
+ *                        Write Functions                               *
+ ***********************************************************************/
+
+export const updateLocker = (key, msg) => new Promise((resolve, reject) => {
+  console.log(`   Updating locker: ${key}`);
+
+  lockFile.lock(DATASTORE, retryOptions)
+  .then(release => {
+    let newMsgData = msgData;
+    newMsgData[key] = msg;
+
+    updateData(newMsgData)
+    .then(() => {
+      msgData = newMsgData;
+      resolve();
+    }).catch(err => {
+      reject(new Error(`Update to DATASTORE failed`));
+    })
+    release();
+  }).catch(err => {
+    reject(new Error(`Lock to update DATASTORE failed: ${err}`));
+  })
+});
+
+export const updateAll = (data) => new Promise ((resolve, reject) => {
+  console.log(`   Updating all lockers`);
+
+  lockFile.lock(DATASTORE, retryOptions)
+  .then(release => {
+    updateData(data)
+    .then(() => {
+      msgData = data;
+      resolve();
+    }).catch(err => {
+      reject(new Error(`Update to DATASTORE failed`));
+    })
+    release();
+  }).catch(err => {
+    reject(new Error(`Lock to update DATASTORE failed: ${err}`));
+  })
+});
+
+const updateData = (data) => new Promise((resolve, reject) => {
+  try {
+    fs.writeFile(DATASTORE, JSON.stringify(data), (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    })
+    resolve();
+  } catch(err) {
+    reject(new Error(`Update to DATASTORE failed: ${err}`));
+  }
+})
+
+/************************************************************************
+ *                        Read Functions                                *
+ ***********************************************************************/
+
+export const readLocker = (locker) => {
+  if (locker in msgData) {
+    return msgData[locker];
+  }
+  return "";
+}
+
+export const readAll = () => msgData;
+
+export const loadData = () => new Promise((resolve, reject) => {
+  console.log("   Loading DATASTORE...");
+
+  lockFile.lock(DATASTORE, retryOptions)
+  .then(release => {
+    try {
+      msgData = JSON.parse(fs.readFileSync(DATASTORE, 'utf-8'));
+      resolve();
+    } catch(err) {
+      reject(new Error(`Unable to read from DATASTORE: ${err}`));
+    }
+    release();
+  }).catch(err => {
+    reject(new Error(`Lock to read DATASTORE failed: ${err}`));
+  })
+})
